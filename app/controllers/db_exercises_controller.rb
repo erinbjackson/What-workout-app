62c08bc6ca605fb34541dc0481e1dc44cd0da23a
Erin Jackson <erinebjackson@gmail.com>
2022-04-12 18:00:12 -0700

    refactors the code in the db_exercises controller to work much faster

--- app/controllers/db_exercises_controller.rb
+++ app/controllers/db_exercises_controller.rb
@@ -6,30 +6,35 @@ class DbExercisesController < ApplicationController
   end
   
   def show
-    exercises = DbExercise.all 
-    filtered_exercises = []
-    final_workouts = []
-    exercises.each do |exercise| 
-      equip_arr = params[:equip_arr]
-      i = 0
-      while i < equip_arr.length
-        if exercise[:equipment] == equip_arr[i]
-          filtered_exercises << exercise
-        end
-        i += 1
-      end
-    end
-    filtered_exercises.each do |filtered_exercise|
-      targets = params[:targets]
-      t = 0
-      while t < targets.length
-        if filtered_exercise[:target] == targets[t]
-          final_workouts << filtered_exercise
-        end
-        t += 1
-      end
-    end
-    render json: final_workouts.shuffle.take(params[:exercise_count].to_i)
+   
+    exercises2 = DbExercise.search(
+      params[:equip_arr],
+      params[:targets],
+      params[:exercise_count]
+    )
+    # filtered_exercises = []
+    # final_workouts = []
+    # exercises.each do |exercise| 
+    #   equip_arr = params[:equip_arr]
+    #   i = 0
+    #   while i < equip_arr.length
+    #     if exercise[:equipment] == equip_arr[i]
+    #       filtered_exercises << exercise
+    #     end
+    #     i += 1
+    #   end
+    # end
+    # filtered_exercises.each do |filtered_exercise|
+    #   targets = params[:targets]
+    #   t = 0
+    #   while t < targets.length
+    #     if filtered_exercise[:target] == targets[t]
+    #       final_workouts << filtered_exercise
+    #     end
+    #     t += 1
+    #   end
+    # end
+    render json: exercises2
   end
  
 end
--- app/models/db_exercise.rb
+++ app/models/db_exercise.rb
@@ -7,4 +7,10 @@ class DbExercise < ApplicationRecord
     name.gsub! "sissy ", "lean back "
     name
   end
+
+  def self.search(equip_arr, target, exercise_count )
+    where(target: target, equipment: equip_arr).order("RANDOM()").limit(exercise_count)
+    
+    
+  end
 end
