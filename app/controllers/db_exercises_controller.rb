a965add593a40d2db471b37b2550531cc37f610c
Erin Jackson <erinebjackson@gmail.com>
2022-04-12 18:02:14 -0700

    removes commented out code from db_exercises controller

--- app/controllers/db_exercises_controller.rb
+++ app/controllers/db_exercises_controller.rb
@@ -12,28 +12,7 @@ class DbExercisesController < ApplicationController
       params[:targets],
       params[:exercise_count]
     )
-    # filtered_exercises = []
-    # final_workouts = []
-    # exercises.each do |exercise| 
-    #   equip_arr = params[:equip_arr]
-    #   i = 0
-    #   while i < equip_arr.length
-    #     if exercise[:equipment] == equip_arr[i]
-    #       filtered_exercises << exercise
-    #     end
-    #     i += 1
-    #   end
-    # end
-    # filtered_exercises.each do |filtered_exercise|
-    #   targets = params[:targets]
-    #   t = 0
-    #   while t < targets.length
-    #     if filtered_exercise[:target] == targets[t]
-    #       final_workouts << filtered_exercise
-    #     end
-    #     t += 1
-    #   end
-    # end
+    
     render json: exercises2
   end
  
